21d539198378d735e5145c62bbb9bd557346f393
ejMina226 <118474890+ejMina226@users.noreply.github.com>
2023-10-20 16:18:42 +0100

    Update tests

--- automation/services/mina-bp-stats/leaderboard-bot/minanet_app/Validation/helper.py
+++ automation/services/mina-bp-stats/leaderboard-bot/minanet_app/Validation/helper.py
@@ -1,5 +1,4 @@
-
-def filter_state_hash_percentage(df, p=50):
+def filter_state_hash_percentage(df, p=0.34):
     state_hash_list = df['state_hash'].value_counts().sort_values(ascending=False).index.to_list()
     # get 34% number of blk in given batch
     total_unique_blk = df['block_producer_key'].nunique()
@@ -11,3 +10,32 @@ def filter_state_hash_percentage(df, p=50):
         if blk_count >= percentage_result:
             good_state_hash_list.append(s)
     return good_state_hash_list
+
+
+def create_graph(batch_df, p_selected_node_df, c_selected_node, p_map):
+    batch_graph = nx.DiGraph()
+    parent_hash_list = batch_df['parent_state_hash'].unique()
+    state_hash_list = set(list(batch_df['state_hash'].unique()) + list(p_selected_node_df['state_hash'].values))
+    selected_parent = [parent for parent in parent_hash_list if parent in state_hash_list]
+    """ t1=[w[42:] for w in list(p_selected_node_df['state_hash'].values)]
+    t2=[w[42:] for w in c_selected_node]
+    t3=[w[42:] for w in state_hash_list]
+    batch_graph.add_nodes_from(t1)
+    batch_graph.add_nodes_from( t2)
+    batch_graph.add_nodes_from(t3) """
+
+    batch_graph.add_nodes_from(list(p_selected_node_df['state_hash'].values))
+    batch_graph.add_nodes_from(c_selected_node)
+    batch_graph.add_nodes_from(state_hash_list)
+
+    for row in batch_df.itertuples():
+        state_hash = getattr(row, 'state_hash')
+        parent_hash = getattr(row, 'parent_state_hash')
+
+        if parent_hash in selected_parent:
+            batch_graph.add_edge(parent_hash, state_hash)
+
+    #  add edges from previous batch nodes
+    batch_graph.add_edges_from(p_map)
+
+    return batch_graph
--- automation/services/mina-bp-stats/leaderboard-bot/minanet_app/test.py
+++ automation/services/mina-bp-stats/leaderboard-bot/minanet_app/test.py
@@ -4,10 +4,21 @@ from Validation.helper import filter_state_hash_percentage
 import pandas as pd
 
 class TestingGraphmethods(unittest.TestCase):
-    def test_filter_state_hash(self):
-        master_state_hash = pd.DataFrame([['dhdhdhdh', 'hfhf']], columns=['state_hash', 'block_producer_key'])
+    def test_filter_state_hash_single(self):
+        master_state_hash = pd.DataFrame([['statehash1', 'block_producer_key_1']], columns=['state_hash', 'block_producer_key'])
         output = filter_state_hash_percentage(master_state_hash)
-        self.assertEqual(output, [])
+        self.assertEqual(output, ['statehash1'])
+
+    def test_filter_state_hash_multi(self):
+        master_state_hash = pd.DataFrame([['statehash1', 'block_producer_key_1'], ['statehash1', 'block_producer_key_2'], ['statehash2', 'block_producer_key_3']], columns=['state_hash', 'block_producer_key'])
+        output = filter_state_hash_percentage(master_state_hash)
+        self.assertEqual(output, ['statehash1'])
+
+    # batch weight
+
+    # queue List
+
+    # graph
 
 if __name__ == '__main__':
     unittest.main()
