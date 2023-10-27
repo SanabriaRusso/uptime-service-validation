2cb63cee6d268bb4cff7ae8d197eb9e96caa657a
ejMina226 <118474890+ejMina226@users.noreply.github.com>
2023-10-27 15:49:00 +0100

    Update tests

--- automation/services/mina-bp-stats/leaderboard-bot/minanet_app/Validation/zkValidator_test.py
+++ automation/services/mina-bp-stats/leaderboard-bot/minanet_app/Validation/zkValidator_test.py
@@ -5,10 +5,11 @@ import os
 class TestingZkValidator(unittest.TestCase):
     def test_get_validate_state_hash(self):
         print ("New file", os.path.join(os.path.dirname(__file__), "test_data"))
-        file_list = ['blocks-Y6uq5BokFR7SaDjxzNsYyp6bk21sib5FN51fKRThxTMgQKM4gF.dat']
+        file_list = ['2021-08-25T18:51:45Z-B62qrQiw9JhUumq457sMxicgQ94Z1WD9JChzJu19kBE8Szb5T8tcUAC.json']
         combine_list=[]
         output = get_validate_state_hash(file_list,combine_list)
         self.assertEqual(output, [])
         
 if __name__ == '__main__':
     unittest.main()
+# ZkDofMkCMu23hRuPTDviE8ej5pDAbyS24qw7NFbQ4mS9H5FUc4
