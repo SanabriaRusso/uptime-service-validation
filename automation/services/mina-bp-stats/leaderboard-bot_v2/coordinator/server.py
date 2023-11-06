b3c010dc05cf3b7291b373b9dbaf7caff4b87fd2
ejMina226 <118474890+ejMina226@users.noreply.github.com>
2023-11-06 16:55:38 +0000

    Updated DB

--- automation/services/mina-bp-stats/leaderboard-bot_v2/coordinator/server.py
+++ automation/services/mina-bp-stats/leaderboard-bot_v2/coordinator/server.py
@@ -1,54 +1,50 @@
-import logging
-from http.server import BaseHTTPRequestHandler
-import urllib.parse
-import time
 from kubernetes import client
-import threading
 import os
+from datetime import datetime
 
 # Set environment variables
 # Get the values of your-image and tag from environment variables
 worker_image = os.environ.get("WORKER_IMAGE", "busybox")
 worker_tag = os.environ.get("WORKER_TAG", "latest")
 
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    def create_and_monitor_pod(self, index):
-        job_name = f"job-{time.strftime('%y-%m-%d-%H-%M')}-{index}" # ZKValidator
-
-        job = client.V1Job(
-            metadata=client.V1ObjectMeta(name=job_name),
-            spec=client.V1JobSpec(
-                template=client.V1PodTemplateSpec(
-                    spec=client.V1PodSpec(
-                        containers=[
-                            client.V1Container(
-                                name="zk-validator",
-                                image=f"{worker_image}:{worker_tag}",
-                                command=["sleep", "10"],
-                                env=[
-                                    client.V1EnvVar(name="JobName", value="value1"),
-                                    client.V1EnvVar(name="FileBatchList", value='[]'),
-                                ],
-                                image_pull_policy="Always",  # Set the image pull policy here
+def setUpValidatorPods(time_intervals, jobs, logging, worker_image, worker_tag):
+    # Create a Kubernetes API client
+    api = client.BatchV1Api()
+    for index, mini_batch in enumerate(time_intervals):
+                    job_name = f"zk-validator-{datetime.now().strftime('%y-%m-%d-%H-%M')}-{index}" # ZKValidator
+                    jobs.append(job_name)
+                    job = client.V1Job(
+                        metadata=client.V1ObjectMeta(name=job_name),
+                        spec=client.V1JobSpec(
+                            template=client.V1PodTemplateSpec(
+                                spec=client.V1PodSpec(
+                                    containers=[
+                                        client.V1Container(
+                                            name="zk-validator",
+                                            image=f"{worker_image}:{worker_tag}",
+                                            command=["sleep", "10"],
+                                            env=[
+                                                client.V1EnvVar(name="JobName", value=f"{job_name}"),
+                                                client.V1EnvVar(name="FileBatchList", value=f'{mini_batch}'),
+                                            ],
+                                            image_pull_policy="Always",  # Set the image pull policy here
+                                        )
+                                    ],
+                                    restart_policy="Never",
+                                )
                             )
-                        ],
-                        restart_policy="Never",
+                        )
                     )
-                )
-            )
-        )
+                    namespace = open("/var/run/secrets/kubernetes.io/serviceaccount/namespace").read()
+                    api.create_namespaced_job(namespace, job)
 
-        namespace = open("/var/run/secrets/kubernetes.io/serviceaccount/namespace").read()
-        api.create_namespaced_job(namespace, job)
-
-        logging.info(f"Launching pod {index}")
-
-        start_time = time.time()
-        while True:
+                    logging.info(f"Launching pod {index}")
+                              
+    while True:
+        for job_name in jobs:
             pod = api.read_namespaced_job(job_name, namespace)
             if pod.status.succeeded is not None and pod.status.succeeded > 0:
-                end_time = time.time()
-                self.wfile.write(f"Pod {index} completed in {end_time - start_time} seconds\n".encode('utf-8'))
-                logging.info(f"Pod {index} completed in {end_time - start_time} seconds")
-                break
-            time.sleep(1)
+                logging.info(f"Pod {index} completed at {datetime.now()}")
+                jobs.remove(job_name)
+        if len(jobs)==0:
+            break
--- automation/services/mina-bp-stats/leaderboard-bot_v2/database/createDB.sql
+++ automation/services/mina-bp-stats/leaderboard-bot_v2/database/createDB.sql
@@ -2,10 +2,9 @@
 DROP TABLE IF EXISTS bot_logs;
 CREATE TABLE bot_logs (
 	id SERIAL PRIMARY KEY, 
-	name_of_file CHARACTER VARYING, 
-	processing_time BIGINT, 
+	processing_time DOUBLE PRECISION, 
 	files_processed INT, 
-	file_timestamps TIMESTAMP, 
+	file_timestamps TIMESTAMPTZ(6), 
 	batch_start_epoch BIGINT, 
 	batch_end_epoch BIGINT
 );
@@ -13,8 +12,9 @@ CREATE TABLE bot_logs (
 DROP TABLE IF EXISTS statehash;
 CREATE TABLE statehash (
 	id SERIAL PRIMARY KEY,
-	values CHARACTER VARYING UNIQUE
+	value TEXT
 );
+CREATE UNIQUE INDEX uq_state_hash ON statehash USING btree (value);
 
 DROP TABLE IF EXISTS botlogs_statehash;
 CREATE TABLE botlogs_statehash (
@@ -22,7 +22,7 @@ CREATE TABLE botlogs_statehash (
 	parent_statehash_id INT, 
 	statehash_id INT, 
 	weight INT NOT NULL, 
-	bot_log_id int,
+	bot_log_id INT,
 	CONSTRAINT fk_parent_statehash
 		FOREIGN KEY(parent_statehash_id) 
 		REFERENCES statehash(id),
@@ -37,21 +37,24 @@ CREATE TABLE botlogs_statehash (
 DROP TABLE IF EXISTS nodes;
 CREATE TABLE nodes (
 	id SERIAL PRIMARY KEY,
-	block_producer_key CHARACTER VARYING UNIQUE,
-	updated_at TIMESTAMP NOT NULL,
+	block_producer_key TEXT,
+	updated_at TIMESTAMPTZ(6),
 	score INT,
-	score_percent NUMERIC(6,2)
+	score_percent NUMERIC(6,2),
+	discord_id TEXT,
+	email_id TEXT,
+	application_status BOOLEAN,
 );
 
 DROP TABLE IF EXISTS points;
 CREATE TABLE points (
 	id SERIAL PRIMARY KEY,
-	file_name CHARACTER VARYING NOT NULL,
-	file_timestamps TIMESTAMP NOT NULL, 
-	blockchain_epoch BIGINT NOT NULL, 
-	blockchain_height BIGINT NOT NULL,
-    amount INT NOT NULL, 
-	created_at TIMESTAMP NOT NULL,
+	file_name TEXT,
+	file_timestamps TIMESTAMPTZ(6), 
+	blockchain_epoch BIGINT, 
+	blockchain_height BIGINT,
+    amount INT, 
+	created_at TIMESTAMPTZ(6),
 	node_id INT,
 	bot_log_id INT,
 	statehash_id INT
@@ -70,19 +73,19 @@ CREATE TABLE points (
 DROP TABLE IF EXISTS uptime_file_history;
 CREATE TABLE uptime_file_history (
 	id SERIAL PRIMARY KEY,
-	file_name CHARACTER VARYING, 
-	receivedat TIMESTAMP NOT NULL,
-	receivedfrom TIMESTAMP NOT NULL, 
+	file_name TEXT, 
+	receivedat BIGINT,
+	receivedfrom TEXT, 
 	node_id INT NOT NULL, 
 	block_statehash INT,
     parent_block_statehash INT 
 	nodedata_blockheight BIGINT NOT NULL, 
 	nodedata_slot BIGINT NOT NULL, 
-	file_modified_at TIMESTAMP NOT NULL, 
-	file_created_at TIMESTAMP NOT NULL, 
-	file_generation TIMESTAMP NOT NULL,
-    file_crc32c CHARACTER VARYING NOT NULL, 
-	file_md5_hash CHARACTER VARYING NOT NULL,
+	file_modified_at TIMESTAMP(6), 
+	file_created_at TIMESTAMP(6), 
+	file_generation BIGINT,
+    file_crc32c TEXT, 
+	file_md5_hash TEXT,
 	CONSTRAINT fk_nodes
 		FOREIGN KEY(node_id) 
 		REFERENCES nodes(id),
@@ -93,21 +96,24 @@ CREATE TABLE uptime_file_history (
 		FOREIGN KEY(statehash_id) 
 		REFERENCES statehash(id)
 );
+CREATE INDEX idx_ufh_node_id ON uptime_file_history USING btree (node_id);
 
 DROP TABLE IF EXISTS score_history;
 CREATE TABLE score_history (
 	id SERIAL PRIMARY KEY,
 	node_id INT,
-	score_at TIMESTAMP NOT NULL, 
-	score INT NOT NULL, 
-	score_percent NUMERIC(6,2) NOT NULL,
+	score_at TIMESTAMP(6), 
+	score INT, 
+	score_percent NUMERIC(6,2),
 	CONSTRAINT fk_nodes
 		FOREIGN KEY(node_id) 
 		REFERENCES nodes(id)
 );
+CREATE UNIQUE INDEX uq_sh_node_score_at ON score_history USING btree (node_id, score_at);
+
 
 DROP TABLE IF EXISTS zk_validator;
-CREATE TABLE zkValidator (
+CREATE TABLE zk_validator (
 	id SERIAL PRIMARY KEY,
 	name_of_file CHARACTER VARYING NOT NULL, 
 	state_hash CHARACTER VARYING NULL,
